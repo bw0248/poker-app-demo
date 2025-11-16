f33f56bbed925925fa075d383a6309fc699b2412
bw0248 <bastianweiser024@gmail.com>
2025-11-16 10:31:12 +0100

    flatten cards package

--- composeApp/src/commonMain/kotlin/io/github/bw0248/simple/poker/tech_demo/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package io.github.bw0248.simple.poker.tech_demo
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
